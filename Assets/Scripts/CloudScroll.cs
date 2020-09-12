cd44a298b075bb834a67a70952ac617ff8dd91a3
DavidQuesillo <68030286+DavidQuesillo@users.noreply.github.com>
2020-09-11 22:52:36 -0300

    Scroll de nubes completo
    Está listo el efecto de que las nubes se muevan en el fondo.

---
+++ Assets/Scripts/CloudScroll.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudScroll : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Vector2 ScrollSpeed;
+    public float DestroyTimer = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.AddForce(ScrollSpeed, ForceMode2D.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DestroyTimer -= Time.deltaTime;
+        if (DestroyTimer <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/CloudSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudSpawner : MonoBehaviour
+{
+    public float SpawnTimer;
+    public float TimerMin;
+    public float TimerMax;
+    public GameObject PrefabCloud;
+    private Vector3 SpawnPosition = new Vector3 (0, -6.5f, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SpawnTimer -= Time.deltaTime;
+        if (SpawnTimer <= 0)
+        {
+            SpawnPosition.x = Random.Range(-3f, 3f);
+            Instantiate(PrefabCloud, SpawnPosition, new Quaternion (0f, 0f, 0f, 0f));
+            SpawnTimer = Random.Range(TimerMin, TimerMax);
+        }
+    }
+}

a6d1970c3dd7413c34440b3940cd79f1448d7a3d
DavidQuesillo <68030286+DavidQuesillo@users.noreply.github.com>
2020-09-12 01:48:56 -0300

    Movimiento jugador listo
    Botones en pantalla para moverse a izquiera y derecha y límites físicos al movimiento horizontal dell jugador para que no se salga de la pantalla

---
+++ Assets/Scripts/LeftButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class LeftButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+
+{
+    public bool left = false;
+
+
+    //Estas dos funciones acompañadas de los handlers arriba permiten que el botón
+    //detecte cuando está siendo sostenido en lugar de solo soltado.
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        left = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        left = false;
+    }
+}
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    public Rigidbody2D rb;
+    public Vector2 MoveSpeed;
+    public LeftButton ButtonL;
+    public RightButton ButtonR;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,13 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (ButtonL.left == true)
+        {
+            rb.AddForce(MoveSpeed, ForceMode2D.Force);
+        }
+        if (ButtonR.right == true)
+        {
+            rb.AddForce(-MoveSpeed, ForceMode2D.Force);
+        }
     }
 }
